555f4e1a16ee21545b5bcd64ca7e5ecf0b02ca3d
Dharshu2050 <dharshinieaswaran52@gmail.com>
2024-09-02 21:02:47 +0530

    change

---
+++ app.py
@@ -0,0 +1,20 @@
+# from flask import Flask, render_template ,request
+
+# app=Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     return"<h1>helo</h1>"
+
+# @app.route('/reg')
+# def ht():
+#     return render_template("reg.html")
+
+# @app.route('/sub', methods=['POST'])
+# def sumbit():
+#     #form la irunthu get panrom demo 
+#     dmo=request.form['name']
+#     return dmo
+
+# if __name__ =="__main__":
+#     app.run(debug=True)
