914e578b64781a283fe4a079372403dabbce0caf
Dharshu2050 <dharshinieaswaran52@gmail.com>
2024-09-03 22:34:49 +0530

    changes

--- app.py
+++ app.py
@@ -1,20 +1,30 @@
-# from flask import Flask, render_template ,request
+from flask import Flask, render_template ,request
 
-# app=Flask(__name__)
+app=Flask(__name__)
 
-# @app.route('/')
-# def home():
-#     return"<h1>helo</h1>"
+@app.route('/')
+def home():
+    return render_template("index.html")
 
-# @app.route('/reg')
-# def ht():
-#     return render_template("reg.html")
+@app.route('/reg')
+def ht():
+    return render_template("reg.html")
 
-# @app.route('/sub', methods=['POST'])
-# def sumbit():
-#     #form la irunthu get panrom demo 
-#     dmo=request.form['name']
-#     return dmo
+@app.route('/sub', methods=['POST'])
+def sumbit():
+    #form la irunthu get panrom demo 
+    name1=request.form['name1']
+    name2=request.form['name2']
+    gender1=request.form['gender']
+    gender2=request.form['gender']
+    email=request.form['email']
+    phonenumber=request.form['phonenumber']
+    address=request.form['address']
+    school=request.form['sc-name']
+    year=request.form['year']
+    yes=request.form['yes']
+    no=request.form['no']
+    return name1+""+name2
 
-# if __name__ =="__main__":
-#     app.run(debug=True)
+if __name__ =="__main__":
+    app.run(debug=True)
